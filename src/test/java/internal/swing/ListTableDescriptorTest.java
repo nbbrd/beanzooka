3e154640d475f5de7bb8479ebf9278691fe44a50
Philippe Charles <philippe.charles@nbb.be>
2022-03-21 10:13:37 +0100

    Migrate to junit5

--- src/test/java/internal/swing/ListTableDescriptorTest.java
+++ src/test/java/internal/swing/ListTableDescriptorTest.java
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import static org.assertj.core.api.Assertions.*;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  *
